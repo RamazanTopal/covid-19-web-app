7a890dba51e0faca468f6bde60c7fd95a0f853fc
ramazantopal <ranbo_1997@hotmail.com>
2021-02-10 09:02:54 +0300

    email and admin panel created

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, escape, request,url_for,render_template,abort,session,redirect,request
-from covid import Covid
+from flask import Flask, escape, request,url_for,render_template,abort,session,redirect,request#flask kütüphanesi
+from covid import Covid#Covid kütüphanesi
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin#admin ekranı
+from flask_admin.contrib.sqla import ModelView#admin ekranı
+from flask_mail import Mail,Message
 import sqlalchemy#404 hatalarını karşılaması için
 app = Flask(__name__)
+#Veritabani baglantisi
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1346795432120@localhost/covid19'
 app.config['SECRET_KEY'] = 'ramazansecretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 admin=Admin(app)
-
+#email
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=465
+app.config['MAIL_USERNAME']='email'
+app.config['MAIL_PASSWORD']='sifre'
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+mail=Mail(app)
 
 
 class Makale(db.Model):
@@ -29,8 +38,9 @@ class ModelViewKıyasla(ModelView):
             abort(403)
 admin.add_view(ModelViewKıyasla(Makale,db.session))
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
+    #Covid 19 Kütüphanesi
     covid = Covid()
     toplam = covid.get_total_confirmed_cases()
     toplam1 = "{sayi:,}".format(sayi=toplam)
@@ -39,6 +49,16 @@ def index():
     olu = covid.get_total_deaths()
     olu1 = "{olenler:,}".format(olenler=olu)
     turkey_cases = covid.get_status_by_country_name("turkey")
+    #Email islemleri
+    if request.method == "POST":
+        isim=request.form["isim"]
+        email=request.form["email"]
+        mesaj=request.form["mesaj"]
+
+        message=Message(isim,sender=email,recipients=["ranbo432@gmail.com"])
+        message.body=mesaj+"\n"+email
+        mail.send(message)
+        return render_template('index.html',toplam=toplam1,iyilesen=iyilesen1,olu=olu1,turkey_cases=turkey_cases,success="Mesaj gönderildi")
     return render_template('index.html',toplam=toplam1,iyilesen=iyilesen1,olu=olu1,turkey_cases=turkey_cases)
 
 
